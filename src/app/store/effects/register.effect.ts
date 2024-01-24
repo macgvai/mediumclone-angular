0ad02df375616d7a08e19addb2ad4a8acd7742b0
Victor <st.viktorr@gmail.com>
2024-01-24 15:29:03 +0300

    persistance servise и redirect

--- src/app/store/effects/register.effect.ts
+++ src/app/store/effects/register.effect.ts
@@ -1,34 +1,54 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { registerAction, registerFailureAction, registerSuccessAction } from "../actions/register.action";
-import { catchError, map, of, switchMap } from "rxjs";
+import { catchError, map, of, switchMap, tap } from "rxjs";
 import { RegisterService } from "../../register/services/register.service";
 import { CurrentUserInterface } from "../../shared/types/currentUser.interface";
 import { HttpErrorResponse } from "@angular/common/http";
+import { PresistanceServices } from "../../shared/services/persistance.service";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class RegisterEffect {
-    register$ = createEffect(() => 
-        this.actions$.pipe(
-            ofType(registerAction),
-            switchMap(({request}) => {
-                return this.registerService.register(request).pipe(
-                    map((currentUser: CurrentUserInterface) => {
-                        console.log('a')
-                        return registerSuccessAction({currentUser})
-                    }),
-                    catchError((errorResponse: HttpErrorResponse) => {
-                        // console.log('b', e)
+    register$ = createEffect(
+        () => 
+            this.actions$.pipe(
+                ofType(registerAction),
+                switchMap(({request}) => {
+                    return this.registerService.register(request).pipe(
+                        map((currentUser: CurrentUserInterface) => {
+                            // window.localStorage.setItem('accessToken', currentUser.token)
+                            this. presistance.set('accessToken', currentUser.token)
+                            return registerSuccessAction({currentUser})
+                        }),
+                        catchError((errorResponse: HttpErrorResponse) => {
+                            // console.log('b', e)
 
-                        return of(registerFailureAction({errors: errorResponse.error.errors }))
-                    })
-                )
-            })
-        ))
+                            return of(registerFailureAction({errors: errorResponse.error.errors }))
+                        })
+                    )
+                })
+            )
+    )
+
+    redirectAfetrSubmit$ = createEffect(
+        () => 
+            this.actions$.pipe(
+                ofType(registerSuccessAction),
+                tap(() => {
+                    this.router.navigateByUrl('/')
+                    console.log('tap')
+                })
+            )
+        ,
+        {dispatch: false}
+    )
 
     constructor(
         private actions$: Actions, 
-        private registerService: RegisterService
+        private registerService: RegisterService,
+        private presistance: PresistanceServices,
+        private router: Router
     ) {}
 }
 
