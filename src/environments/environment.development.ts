be4daff2e6cf4a50cfa49897daf66f4b12547510
Victor <st.viktorr@gmail.com>
2024-02-05 10:20:59 +0300

    local api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://conduit.productionready.io/api'
+    // apiUrl: 'https://conduit.productionready.io/api'
+    apiUrl: 'http://localhost:3000/api',
 };
